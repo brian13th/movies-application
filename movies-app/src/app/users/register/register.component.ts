e4977a880385aee20094d99ade87c4fe47678666
brian13th <brian13th13@gmail.com>
2020-10-22 21:55:41 +0300

    started registration form

---
+++ movies-app/src/app/users/register/register.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  submitData(){
+    console.log(this.registrationForm.value)
+  }
+
+  registrationForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    repassword: new FormControl('', [Validators.required]),
+  })
+
+  get firstName() {
+    return this.registrationForm.get('firstName');
+  }
+  get lastName() {
+    return this.registrationForm.get('lastName');
+  }
+  get username() {
+    return this.registrationForm.get('username');
+  }
+  get password() {
+    return this.registrationForm.get('password');
+  }
+
+  get repassword() {
+    return this.registrationForm.get('repassword');
+  }
+
+}
