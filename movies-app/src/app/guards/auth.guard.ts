c056257bb2e3175c806fdb530747c6b82683f65d
brian13th <brian13th13@gmail.com>
2020-10-24 02:57:04 +0300

    started AuthGuard

--- movies-app/src/app/guards/auth.guard.ts
+++ movies-app/src/app/guards/auth.guard.ts
@@ -6,10 +6,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    
     return true;
   }
-  
+
 }
